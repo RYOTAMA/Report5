6d2b6e8aa30356cc36b553ce7aad7f78037defd0
Matsushita Ryota <e165710@ie.u-ryukyu.ac.jp>
2016-12-12 22:02:12 +0900

    ステップ2終了

--- src/main/jp/ac/uryukyu/ie/e165710/Report5_1.java
+++ src/main/jp/ac/uryukyu/ie/e165710/Report5_1.java
@@ -1,8 +1,15 @@
 package jp.ac.uryukyu.ie.e165710;
 
 public class Report5_1 {
-    public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println("NullPointerExceptionが発生しました");
+            e.printStackTrace();
+            return;
+        }
     }
 }
