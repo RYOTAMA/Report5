5c97d0b4937ecf9438b51232bdd31c27fb22632b
Matsushita Ryota <e165710@ie.u-ryukyu.ac.jp>
2016-12-12 22:47:26 +0900

    ステップ3終了

--- src/main/jp/ac/uryukyu/ie/e165710/Report5_1.java
+++ src/main/jp/ac/uryukyu/ie/e165710/Report5_1.java
@@ -3,7 +3,8 @@ package jp.ac.uryukyu.ie.e165710;
 public class Report5_1 {
     public static void main(String[] args) {
         try {
-            String str = null;
+            String str =　3．14;
+            double value = Double.parseDouble(str);
             System.out.println(str.length());
         }
         catch (NullPointerException e){
